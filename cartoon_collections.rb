0667e2e93faba1d7c4ea7a1645d88c83e22d0d26
Erin Mettille <ecmettille@gmail.com>
2020-04-17 18:28:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  calls.split.map { |word| "#{word.capitalize}!" }.join(" ")
+  calls.map { |word| "#{word.capitalize}!" }
 end
 
 def long_planeteer_calls# code an argument here
