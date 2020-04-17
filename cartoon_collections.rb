52df66e5e4224b0c9503589b3feb98580d5eac66
Erin Mettille <ecmettille@gmail.com>
2020-04-17 18:48:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(longcall)
   longcall.any? { |word| word.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
+  food.include? { |yummy| }
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
